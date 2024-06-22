18446d4c46fce8f2a1957d960f3d5c669f318296
amansikarwar1988 <94310072+amansikarwar1988@users.noreply.github.com>
2024-06-22 13:18:03 +0530

    Create TestController.java

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+// Java Program to Illustrate DemoController File 
+
+// Importing package in this code module 
+package com.example.demo.controller; 
+// Importing required classes 
+import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.GetMapping
+
+@RestController
+public class DemoController { 
+
+	@GetMapping("/hello") 
+	public String helloGFG() 
+	{ 
+		return "Hello GeeksForGeeks"; 
+	} 
+}
